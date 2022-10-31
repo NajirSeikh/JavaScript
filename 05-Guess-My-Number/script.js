f47b0cd11b8e7ee1c29604a7c5b31b28a9625170
NajirSeikh <ns.soft.97@gmail.com>
2022-10-31 18:48:31 +0530

    manipulating CSS styles

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -18,10 +18,19 @@ document.querySelector('.check').addEventListener('click', () => {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess);
 
+  // When there is no input
   if (!guess) {
     document.querySelector('.message').textContent = '⛔ No Number';
+
+    // When player wins
   } else if (guess == secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number';
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+
+    document.querySelector('.number').style.width = '30rem';
+
+    // When guess is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '📈 Too High';
@@ -31,6 +40,8 @@ document.querySelector('.check').addEventListener('click', () => {
       document.querySelector('.message').textContent = '💥 You lost the game!';
       document.querySelector('.score').textContent = 0;
     }
+
+    // When guess is too low
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '📉 Too Low';
