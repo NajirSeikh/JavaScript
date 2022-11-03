59e0a93a88d70630478aab214b6764129cc48aa1
NajirSeikh <ns.soft.97@gmail.com>
2022-11-03 19:41:48 +0530

    implementing highscore

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -11,8 +11,8 @@
 // console.log(document.querySelector('.guess').value);
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
-
 let score = 20;
+let highScore = 0;
 
 document.querySelector('.check').addEventListener('click', () => {
   const guess = Number(document.querySelector('.guess').value);
@@ -25,12 +25,14 @@ document.querySelector('.check').addEventListener('click', () => {
     // When player wins
   } else if (guess == secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number';
-
     document.querySelector('.number').textContent = secretNumber;
-
     document.querySelector('body').style.backgroundColor = '#60b347';
-
     document.querySelector('.number').style.width = '30rem';
+
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
 
     // When guess is too high
   } else if (guess > secretNumber) {
