c667ac62dc669a0bfcba9ae994abd8d923fd3d22
NajirSeikh <ns.soft.97@gmail.com>
2023-04-27 10:12:01 +0530

    Scoping in Practice

--- 08-Behind-the-Scenes/script.js
+++ 08-Behind-the-Scenes/script.js
@@ -1,10 +1,39 @@
 'use strict';
 
-function calAge(birthYear) {
-  const age = 2022 - birthYear;
-  console.log(firstName);
+///////////////////////////////////////
+// Scoping in Practice
+
+function calcAge(birthYear) {
+  const age = 2023 - birthYear;
+  function printAge() {
+    let output = `${firstName}, you are ${age}, born in ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      var millenial = 'true';
+      // Creating NEW variable with same name as outer scope's variable
+      const firstName = 'Munna';
+
+      // Reasssigning outer scope's variable
+      output = 'NEW OUTPUT!';
+
+      const str = `Oh, and you're a millenial, ${firstName}`;
+      console.log(str);
+      function add(a, b) {
+        return a + b;
+      }
+    }
+    // console.log(str);
+    console.log(millenial);
+    // console.log(add(2, 3));
+    console.log(output);
+  }
+  printAge();
+
   return age;
 }
 
 const firstName = 'Najir';
-calAge(1997);
+calcAge(1997);
+// console.log(age);
+// printAge();
