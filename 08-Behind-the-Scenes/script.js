7fa218e686944a23ef909bb8ade5fcef822bf1d6
NajirSeikh <ns.soft.97@gmail.com>
2023-05-07 20:14:42 +0530

    Hoisting and TDZ in Practice

--- 08-Behind-the-Scenes/script.js
+++ 08-Behind-the-Scenes/script.js
@@ -1,8 +1,7 @@
 'use strict';
 
-///////////////////////////////////////
-// Scoping in Practice
-
+//////////////////////////////////////// Scoping in Practice
+/*
 function calcAge(birthYear) {
   const age = 2023 - birthYear;
   function printAge() {
@@ -37,3 +36,48 @@ const firstName = 'Najir';
 calcAge(1997);
 // console.log(age);
 // printAge();
+*/
+
+//////////////////////////////////////// Hoisting in Practice
+
+// Variables
+console.log(me);
+// console.log(job);
+// console.log(year);
+
+var me = 'Najir';
+let job = 'Student';
+const year = 1997;
+
+// Functions
+console.log(addDecl(2, 3));
+// console.log(addExpr(2, 3));
+console.log(addArrow);
+// console.log(addArrow(2, 3));
+
+function addDecl(a, b) {
+  return a + b;
+}
+
+const addExpr = function (a, b) {
+  return a + b;
+};
+
+var addArrow = (a, b) => a + b;
+
+// Example
+if (!numProducts) deleteShoppingCart();
+
+var numProducts = 10;
+
+function deleteShoppingCart() {
+  console.log('All products deleted!');
+}
+
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log(x === window.x);
+console.log(y === window.y);
+console.log(z === window.z);
