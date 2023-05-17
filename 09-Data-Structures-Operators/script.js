e7f895dabe15c0a4054c1769ce6e800c89eeb2db
NajirSeikh <ns.soft.97@gmail.com>
2023-05-17 10:04:15 +0530

    The Spread Operator(...)

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -33,13 +33,67 @@ const restaurant = {
     address,
   }) {
     console.log(
-      `Odere recieved! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+      `Oder recieved! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
+  },
+
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(
+      `Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`
     );
   },
 };
 
 /////////////////////////////////////////////
-// Destructuring Objects
+// The Spread Operator(...) Dated-17.05.2023
+const arr = [7, 8, 9];
+const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
+console.log(badNewArr);
+
+const newArr = [1, 2, ...arr];
+console.log(newArr);
+
+console.log(...newArr);
+console.log(1, 2, 7, 8, 9);
+
+const newMenu = [...restaurant.mainMenu, 'Gnocci'];
+console.log(newMenu);
+
+// Copy array
+const mainMenuCopy = [...restaurant.mainMenu];
+
+// Join 2 arrays
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+console.log(menu);
+
+// Iterables: arrays, strings, maps, sets, NOT objects
+const str = 'Najir';
+const letters = [...str, ' ', 'S.'];
+console.log(letters);
+console.log(...str);
+// Real-World example
+const ingredients = [
+  // prompt("let's make pasta! Ingredient 1?"),
+  // prompt("let's make pasta! Ingredient 2?"),
+  // prompt("let's make pasta! Ingredient 3?"),
+];
+console.log(ingredients);
+
+// restaurant.orderPasta(ingredients[0], ingredients[1], ingredients[2]);
+restaurant.orderPasta(...ingredients);
+
+// Objects
+const newRestaurant = { foundedIn: 1998, ...restaurant, founder: 'Guiseppe' };
+console.log(newRestaurant);
+
+const restaurantCopy = { ...restaurant };
+restaurantCopy.name = 'Ristorante Roma';
+console.log(restaurantCopy.name);
+console.log(restaurant.name);
+
+/*
+/////////////////////////////////////////////
+// Destructuring Objects Dated-17.05.2023
 
 const { name, openingHours, categories } = restaurant;
 console.log(name, openingHours, categories);
@@ -80,6 +134,7 @@ restaurant.orderDelivery({
   address: 'Via del Sole, 21',
   starterIndex: 1,
 });
+*/
 
 /*
 /////////////////////////////////////////////
