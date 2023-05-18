579d5fa6f7e791cf009a80ca9d785abbba2641be
NajirSeikh <ns.soft.97@gmail.com>
2023-05-19 00:25:27 +0530

    Coding Challenge #1 is done

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -50,8 +50,108 @@ const restaurant = {
   },
 };
 
-/* **************Logical Assgnment Operators - Dated-18.05.2023************** */
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
 
+/* **************Coding Challenge #1 - Dated-18.05.2023************** */
+
+/* 
+We're building a football betting app (soccer for my American friends 😅)!
+
+Suppose we get data from a web service about a certain game (below). In this challenge we're gonna work with the data. So here are your tasks:
+
+1. Create one player array for each team (variables 'players1' and 'players2')
+2. The first player in any player array is the goalkeeper and the others are field players. For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 field players
+3. Create an array 'allPlayers' containing all players of both teams (22 players)
+4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
+5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
+6. Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
+7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, WITHOUT using an if/else statement or the ternary operator.
+
+TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Then, call the function again with players from game.scored
+
+GOOD LUCK 😀
+*/
+
+// Task 1
+// const players1 = game.players[0];
+// const players2 = game.players[1];
+const [players1, players2] = game.players;
+console.log(players1, players2);
+
+// Task 2
+const [gk, ...fieldPlayers] = players1;
+console.log(gk, fieldPlayers);
+
+// Task 3
+const allPlayers = [...players1, ...players2];
+console.log(allPlayers);
+
+// Task 4
+const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+console.log(players1Final);
+
+// Task 5
+// const { team1: team1, x: draw, team2: team2 } = game.odds;
+const {
+  odds: { team1, x: draw, team2 },
+} = game;
+console.log(team1, draw, team2);
+
+// Task 6
+const printGoals = function (...players) {
+  console.log(players);
+  console.log(`${players.length} goals were scored.`);
+};
+
+// printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
+// printGoals('Davies', 'Muller');
+printGoals(...game.scored);
+
+// Task 7
+team1 < team2 && console.log('Team 1 is more likely to win.');
+team1 > team2 && console.log('Team 2 is more likely to win.');
+
+/* **************Logical Assgnment Operators - Dated-18.05.2023************** */
+/*
 const rest1 = {
   name: 'Capri',
   // numGuests: 20,
@@ -83,7 +183,7 @@ console.log(rest1);
 console.log(rest2);
 
 /* **************The Nullish  Coalescing Operator (??) - Dated-18.05.2023************** */
-
+/*
 restaurant.numGuests = 0;
 const guests = restaurant.numGuests || 10;
 console.log(guests);
@@ -94,6 +194,7 @@ console.log(guestCorrect);
 
 /* **************Short Circutting(|| and &&) - Dated-18.05.2023************** */
 // Use ANY data type, return ANY data type, short circutting
+/*
 console.log('---- OR ----');
 console.log(3 || 'Najir');
 console.log(' ' || 'Najir');
@@ -123,7 +224,7 @@ restaurant.orderPizza && restaurant.orderPizza('Mashrooms', 'Spinach');
 
 /* **************Rest Pattern and Parameters - Dated-18.05.2023************** */
 // SPREAD, because on RIGHT side of =
-const arr = [1, 2, ...[3, 4]];
+/*const arr = [1, 2, ...[3, 4]];
 
 // REST, beacause of LEFT side of =
 const [a, b, ...others] = [1, 2, 3, 4, 5];
