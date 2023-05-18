b943f719360d071d7402601de6ddf9573b427303
NajirSeikh <ns.soft.97@gmail.com>
2023-05-18 09:20:16 +0530

    The Nullish Coalescing Operator(??)

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -50,6 +50,18 @@ const restaurant = {
 };
 
 /////////////////////////////////////////////
+// The Nullish  Coalescing Operator (??)  Dated-18.05.2023
+
+restaurant.numGuests = 0;
+const guests = restaurant.numGuests || 10;
+console.log(guests);
+
+// Nullish: null or undefined (NOT 0 or '')
+const guestCorrect = restaurant.numGuests ?? 10;
+console.log(guestCorrect);
+
+/*
+/////////////////////////////////////////////
 // Short Circutting(&& and __)  Dated-18.05.2023
 
 // Use ANY data type, return ANY data type, short circutting
