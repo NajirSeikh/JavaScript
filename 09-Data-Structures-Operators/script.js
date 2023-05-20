6259619902d57b9a4d93c5a8bda929f0e0dd4dd5
NajirSeikh <ns.soft.97@gmail.com>
2023-05-20 18:05:26 +0530

    Working With String-Part 3

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -49,8 +49,60 @@ const restaurant = {
   },
 };
 
-/* **************Working With String-Part 2 - Dated-20.05.2023************** */
+/* **************Working With String-Part 3 - Dated-20.05.2023************** */
 
+// Split() and Slice()
+console.log('a+very+nice+string'.split('+'));
+console.log('Najir Seikh'.split(' '));
+
+const [firstName, lastName] = 'Najir Seikh'.split(' ');
+
+const newName = ['Mr.', firstName, lastName.toUpperCase()].join(' ');
+console.log(newName);
+
+const capitalizeName = function (name) {
+  const names = name.split(' ');
+  const namesUpper = [];
+  for (const n of names) {
+    // namesUpper.push(n[0].toUpperCase() + n.slice(1));
+    namesUpper.push(n.replace(n[0], n[0].toUpperCase()));
+  }
+  console.log(namesUpper.join(' '));
+};
+
+capitalizeName('jessica aan smith davis');
+capitalizeName('najir seikh');
+
+// Padding
+const message = 'Go to gate 23!';
+// console.log(message.padStart(25, '+'));
+console.log(message.padStart(20, '+').padEnd(30, '+'));
+// console.log('Najir'.padStart(25, '+'));
+console.log('Najir'.padStart(20, '+').padEnd(30, '+'));
+
+const maskCreditCard = function (number) {
+  const str = number + '';
+  const last = str.slice(-4);
+  return last.padStart(str.length, '*');
+};
+
+console.log(maskCreditCard(64637836));
+console.log(maskCreditCard(43378463864647384));
+console.log(maskCreditCard('334859493847755774747'));
+
+// Repeat
+const message2 = 'Bad waether... All Departues Delayed... ';
+console.log(message2.repeat(5));
+
+const planesInLine = function (n) {
+  console.log(`There are ${n} planes in line ${'🛩'.repeat(n)}`);
+};
+planesInLine(5);
+planesInLine(3);
+planesInLine(7);
+
+/* **************Working With String-Part 2 - Dated-20.05.2023************** */
+/*
 const airline = 'TAP Air Portugal';
 
 console.log(airline.toLowerCase());
