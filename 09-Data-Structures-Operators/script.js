14442779b8c853b22df2f45ce7198f64d66d4961
NajirSeikh <ns.soft.97@gmail.com>
2023-05-20 13:12:30 +0530

    Maps-Fundamentals

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -49,8 +49,43 @@ const restaurant = {
   },
 };
 
-/* **************Sets - Dated-20.05.2023************** */
+/* **************Maps-Fundamentals - Dated-20.05.2023************** */
 
+const rest = new Map();
+rest.set('name', 'Classico Italiano');
+rest.set(1, 'Firenze, Italy');
+console.log(rest.set(2, 'Lisbon, Portugal'));
+
+rest
+  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'We are open :D')
+  .set(false, 'We are close :(');
+
+console.log(rest.get('name'));
+console.log(rest.get(true));
+console.log(rest.get(1));
+
+const time = 8;
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+console.log(rest.has('categories'));
+rest.delete(2);
+// rest.clear();
+
+// rest.set([1, 2], 'Test');
+const arr = [1, 2];
+rest.set(arr, 'Test');
+rest.set(document.querySelector('h1'), 'Heading');
+console.log(rest);
+console.log(rest.size);
+
+// console.log(rest.get([1, 2]));
+console.log(rest.get(arr));
+
+/* **************Sets - Dated-20.05.2023************** */
+/*
 const orderSet = new Set([
   'Pizza',
   'Pizza',
