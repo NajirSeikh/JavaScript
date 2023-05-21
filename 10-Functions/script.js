13a7a093d44afb784da98cea6fae3287049b9efd
NajirSeikh <ns.soft.97@gmail.com>
2023-05-21 10:56:51 +0530

    The bind Method

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -117,6 +117,7 @@ const greetArr = greeting => name => console.log(`${greeting} ${name}`);
 greetArr('Hi')('Arif');
 */
 /* **************The call and apply Methods - Dated-21.05.2023************** */
+
 const lufthansa = {
   airline: 'Lufthansa',
   iataCode: 'LH',
@@ -171,3 +172,52 @@ console.log(swiss);
 // Same as apply method
 book.call(eurowings, ...flightData);
 console.log(eurowings);
+
+/* **************The bind Method - Dated-21.05.2023************** */
+
+// Bind Method
+// book.call(eurowings, 23, 'Sarah Williams');
+
+const bookEW = book.bind(eurowings);
+const bookLH = book.bind(lufthansa);
+const bookLX = book.bind(swiss);
+
+bookEW(23, 'Steven Williams');
+// console.log(eurowings);
+const bookEW23 = book.bind(eurowings, 23);
+bookEW23('Najir Seikh');
+bookEW23('Jani Mir');
+
+// With Event Listeners
+lufthansa.planes = 300;
+lufthansa.buyPlane = function () {
+  console.log(this);
+
+  this.planes++;
+  console.log(this.planes);
+};
+// lufthansa.buyPlane();
+
+document
+  .querySelector('.buy')
+  .addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+
+// Partial application
+const addTax = (rate, value) => value + value * rate;
+console.log(addTax(0.1, 200));
+
+const addVAT = addTax.bind(null, 0.23);
+// addVAT = value => value + value * 0.23;
+
+console.log(addVAT(100));
+console.log(addVAT(23));
+
+// Same task without Bind() and Arrow() methods
+const addTaxRate = function (rate) {
+  return function (value) {
+    return value + value * rate;
+  };
+};
+const addVAT2 = addTaxRate(0.23);
+console.log(addVAT2(100));
+console.log(addVAT2(23));
