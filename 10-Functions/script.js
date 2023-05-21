e0b22f8c7470825fc6dfa744b7eba298595883d0
NajirSeikh <ns.soft.97@gmail.com>
2023-05-21 08:25:25 +0530

    How Passing Arguments Works - Value VS Reference

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -1,7 +1,7 @@
 /* **************Code by Najir Seikh - 21/05/2023************** */
 'use strict';
 /* **************Default Parameters - Dated-21.05.2023************** */
-
+/*
 const bookings = [];
 
 const createBooking = function (
@@ -29,3 +29,37 @@ createBooking('LH123', 5);
 
 // To skip a parameter, set 'undefined'
 createBooking('LH123', undefined, 1000);
+*/
+/* **************How Passing Arguments Works-Value VS Reference - Dated-21.05.2023************** */
+
+const flight = 'LH234';
+const najir = {
+  name: 'Najir Seikh',
+  passport: 1234567890,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr. ' + passenger.name;
+
+  if (passenger.passport === 1234567890) {
+    alert('Checked in');
+  } else {
+    alert('Wrong passport!');
+  }
+};
+
+// checkIn(flight, najir);
+// console.log(flight);
+// console.log(najir);
+
+//Is the same as doing...
+// const flightNum = flight;
+// const passenger = najir;
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 100000000000);
+};
+
+newPassport(najir);
+checkIn(flight, najir);
