c0060887ea5f9fd2cdcc0ce78dc720730bbab1db
NajirSeikh <ns.soft.97@gmail.com>
2023-05-21 19:37:49 +0530

    Coding Challenge #2 is done

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -400,7 +400,7 @@ present in the environment where the function was created.
 access closed-over variables explicitly. A closure is NOT a tangible JavaScript object.
 */
 /* **************More Closures Examples - Dated-21.05.2023************** */
-
+/*
 // Example 1
 let f;
 
@@ -440,3 +440,26 @@ const boardPassengers = function (n, wait) {
 
 const perGroup = 1000;
 boardPassengers(180, 3);
+
+*/
+/* **************Coding Challenge #2 - Dated-21.05.2023************** */
+/* 
+This is more of a thinking challenge than a coding challenge 🤓
+
+Take the IIFE below and at the end of the function, attach an event listener that changes the color of the selected h1 element ('header') to blue, each time the BODY element is clicked. Do NOT select the h1 element again!
+
+And now explain to YOURSELF (or someone around you) WHY this worked! Take all the time you need. Think about WHEN exactly the callback function is executed, and what that means for the variables involved in this example.
+
+GOOD LUCK 😀
+*/
+
+(function () {
+  console.log('IIFE Function called');
+  const header = document.querySelector('h1');
+  header.style.color = 'red';
+
+  document.querySelector('body').addEventListener('click', () => {
+    console.log('Body clicked');
+    header.style.color = 'blue';
+  });
+})();
