93140985099da1895827d5814a156c7c87d6ffa5
NajirSeikh <ns.soft.97@gmail.com>
2023-05-27 23:50:18 +0530

    More Ways of Creating and Filling Arrays

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -630,7 +630,7 @@ console.log(overallBalance2);
 
 */
 /* **************Sorting Arrays  - 27/05/2023************** */
-
+/*
 // Strings
 const owners = ['Najir', 'Romi', 'Jack', 'Imran', 'Arya'];
 console.log(owners);
@@ -659,3 +659,37 @@ console.log(movements);
 movements.sort((a, b) => b - a);
 
 console.log(movements);
+*/
+/* **************More Ways of Creating and Filling Arrays - 27/05/2023************** */
+
+const arr = [1, 2, 3, 4, 5, 6, 7];
+console.log(new Array(1, 2, 3, 4, 5, 6, 7));
+
+// Emprty arrays + fill method
+const x = new Array(7);
+console.log(x);
+// console.log(x.map(() => 5));
+x.fill(1, 3, 5);
+console.log(x);
+x.fill(1);
+console.log(x);
+
+arr.fill(23, 2, 6);
+console.log(arr);
+
+// Array.from
+const y = Array.from({ length: 7 }, () => 1);
+console.log(y);
+
+const z = Array.from({ length: 7 }, (_, i) => i + 1);
+console.log(z);
+
+labelBalance.addEventListener('click', function () {
+  const movementsUI = Array.from(
+    document.querySelectorAll('.movements__value'),
+    el => Number(el.textContent.replace('€', ''))
+  );
+  console.log(movementsUI);
+
+  const movementsUI2 = [...document.querySelectorAll('.movements__value')];
+});
