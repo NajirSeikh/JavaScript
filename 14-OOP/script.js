6463f2eae1d6a2d73dda82226ebe30be26fe8620
NajirSeikh <ns.soft.97@gmail.com>
2023-05-29 07:21:41 +0530

    Prototypes

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -2,6 +2,7 @@
 'use strict';
 
 /* **************Constructor Functions and the new Operator - 29/05/2023************** */
+
 const Person = function (firstName, birthYear) {
   // Instance properties
   // this.fName = firstName;
@@ -31,3 +32,28 @@ console.log(romi, maha);
 console.log(najir instanceof Person);
 const jay = {};
 console.log(jay instanceof Person);
+
+/* **************Prototypes - 29/05/2023************** */
+console.log(Person.prototype);
+
+Person.prototype.calcAge = function () {
+  console.log(2023 - this.birthYear);
+};
+
+najir.calcAge();
+romi.calcAge();
+
+console.log(najir.__proto__);
+console.log(najir.__proto__ === Person.prototype);
+
+console.log(Person.prototype.isPrototypeOf(najir));
+console.log(Person.prototype.isPrototypeOf(maha));
+console.log(Person.prototype.isPrototypeOf(Person));
+
+// .prototyeOfLinkedObjects
+
+Person.prototype.species = 'Homo Sapiens';
+console.log(najir.species, romi.species);
+
+console.log(najir.hasOwnProperty('firstName'));
+console.log(najir.hasOwnProperty('species'));
