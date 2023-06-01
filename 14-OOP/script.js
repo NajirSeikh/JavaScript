326a09fb2e215b24717d7630aa60e3f085b58ec0
NajirSeikh <ns.soft.97@gmail.com>
2023-06-01 09:43:35 +0530

    Setters and Getters

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -131,14 +131,14 @@ mercedes.accelerate();
 mercedes.brake();
 */
 /* **************ES6 Classes - 30/05/2023************** */
-/*
+
 // Class expression
 // const PersonCl = class{}
 
 // Class declaration
 class PersonCl {
-  constructor(firstName, birthYear) {
-    this.firstName = firstName;
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
     this.birthYear = birthYear;
   }
 
@@ -150,11 +150,26 @@ class PersonCl {
   greet() {
     console.log(`Hey ${this.firstName}`);
   }
+
+  get age() {
+    return 2023 - this.birthYear;
+  }
+
+  // Set a property that already exists
+  set fullName(name) {
+    if (name.includes(' ')) this._fullName = name;
+    else alert(`${name} is not a full name!`);
+  }
+
+  get fullName() {
+    return this._fullName;
+  }
 }
 
-const imran = new PersonCl('Imran', 1997);
+const imran = new PersonCl('Imran Hossain', 1997);
 console.log(imran);
 imran.calcAge();
+console.log(imran.age);
 
 console.log(imran.__proto__ === PersonCl.prototype);
 
@@ -167,8 +182,10 @@ imran.greet();
 // 1. Classes is NOT hoisted
 // 2. Class are first-class citizen
 // 3. Classes are executed in strict mode
-*/
-/* **************Setters and Getters - 31/05/2023************** */
+
+const suman = new PersonCl('Suman Reddy', 1996);
+
+/* **************Setters and Getters - 1/06/2023************** */
 
 const account = {
   owner: 'najir',
