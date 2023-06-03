28079ea14ee06a8a0a9234779de7c159b28a86da
NajirSeikh <ns.soft.97@gmail.com>
2023-06-03 20:50:48 +0530

    Our First AJAX Call: XMLHttpRequest

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -1,6 +1,71 @@
+/* **************Code by Najir Seikh - 3/06/2023************** */
 'use strict';
+/* **************Our First AJAX Call: XMLHttpRequest - 29/05/2023************** */
+/*
+const getCountryData = function (country) {
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.eu/rest/v2/name/${country}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+
+    const html = `
+  <article class="country">
+    <img class="country__img" src="${data.flag}" />
+    <div class="country__data">
+      <h3 class="country__name">${data.name}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${(
+        +data.population / 1000000
+      ).toFixed(1)} people</p>
+      <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+      <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+    </div>
+  </article>
+  `;
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+  });
+};
+
+getCountryData('portugal');
+getCountryData('usa');
+getCountryData('germany');
+*/
 
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
-///////////////////////////////////////
+const request = new XMLHttpRequest();
+request.open('GET', 'https://restcountries.com/v3.1/name/india');
+// request.open('GET', 'https://restcountries.com/v3.1/name/usa');
+request.send();
+// console.log(request.responseText);
+
+request.addEventListener('load', function () {
+  // console.log(this.responseText);
+
+  const [data] = JSON.parse(this.responseText);
+  console.log(data);
+
+  const html = `
+  <article class="country">
+          <img class="country__img" src="${data?.flags?.png}" />
+          <div class="country__data">
+            <h3 class="country__name">${data?.name?.common}</h3>
+            <h6 class="country__region">${data?.capital}</h6>
+            <h4 class="country__region">${data?.region}</h4>
+            <p class="country__row"><span>👫</span>${(
+              +data.population / 1000000
+            ).toFixed(1)}m people</p>
+            <p class="country__row"><span>🗣️</span>${data?.languages?.hin}</p>
+            <p class="country__row"><span>💰</span>${
+              data?.currencies?.INR?.name
+            }</p>
+          </div>
+        </article>`;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+});
