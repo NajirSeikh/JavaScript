e5b8751a0a344b66ae6c3303bbed358c95013de4
NajirSeikh <ns.soft.97@gmail.com>
2023-06-03 10:06:24 +0530

    Encapsulation: Protected Properties and Methods

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -505,7 +505,7 @@ jay.introduce();
 jay.calcAge();
 */
 /* **************Another Class Example - 3/06/2023************** */
-
+/*
 class Account {
   constructor(owner, currency, pin) {
     this.owner = owner;
@@ -549,3 +549,56 @@ acc1.approveLoan(1000);
 
 console.log(acc1);
 console.log(acc1.pin);
+
+*/
+/* **************Encapsulation: Protected Properties and Methods - 3/06/2023************** */
+
+class Account {
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    // Protected property
+    this._pin = pin;
+    this._movements = [];
+    this.locale = navigator.language;
+
+    console.log(`Thanks for opening an account, ${owner}`);
+  }
+
+  // Public intereface
+  getMovements() {
+    return this._movements;
+  }
+
+  deposit(val) {
+    this._movements.push(val);
+  }
+
+  withdraw(val) {
+    this.deposit(-val);
+  }
+
+  _approveLoan(val) {
+    return true;
+  }
+
+  requestLoan(val) {
+    if (this._approveLoan(val)) {
+      this.deposit(val);
+      console.log('Loan approved');
+    }
+  }
+}
+
+const acc1 = new Account('Najir', 'EUR', 1111);
+
+// acc1._movements.push(250);
+// acc1._movements.push(-150);
+acc1.deposit(250);
+acc1.withdraw(140);
+acc1.requestLoan(1000);
+acc1._approveLoan(1000);
+console.log(acc1.getMovements());
+
+console.log(acc1);
+console.log(acc1._pin);
