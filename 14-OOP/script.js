01c2423821065190cf7cd54ad2a3334b334d3c85
NajirSeikh <ns.soft.97@gmail.com>
2023-06-03 10:27:47 +0530

    Encapsulation: Private Class Fields and Methods

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -552,7 +552,7 @@ console.log(acc1.pin);
 
 */
 /* **************Encapsulation: Protected Properties and Methods - 3/06/2023************** */
-
+/*
 class Account {
   constructor(owner, currency, pin) {
     this.owner = owner;
@@ -602,3 +602,82 @@ console.log(acc1.getMovements());
 
 console.log(acc1);
 console.log(acc1._pin);
+
+*/
+/* **************Encapsulation: Private Class Fields and Methods - 3/06/2023************** */
+
+// 1) Public fields
+// 2) Private fields
+// 3) Public methods
+// 4) Private methods
+// (there is also the static veersion)
+
+class Account {
+  // 1) Public fields (instances)
+  locale = navigator.language;
+
+  // 2) Private fields (instances)
+  #movements = [];
+  #pin;
+
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    // Protected property
+    this.#pin = pin;
+    // this._movements = [];
+    // this.locale = navigator.language;
+
+    console.log(`Thanks for opening an account, ${owner}`);
+  }
+
+  // 3) Public methods
+
+  // Public intereface
+  getMovements() {
+    return this.#movements;
+  }
+
+  deposit(val) {
+    this.#movements.push(val);
+  }
+
+  withdraw(val) {
+    this.deposit(-val);
+  }
+
+  requestLoan(val) {
+    // if (this.#approveLoan(val)) {
+    if (this._approveLoan(val)) {
+      this.deposit(val);
+      console.log('Loan approved');
+    }
+  }
+
+  static helper() {
+    console.log('Helper');
+  }
+
+  // 4) Private methods
+  // #approveLoan(val) {
+  _approveLoan(val) {
+    return true;
+  }
+}
+
+const acc1 = new Account('Najir', 'EUR', 1111);
+
+// acc1._movements.push(250);
+// acc1._movements.push(-150);
+acc1.deposit(250);
+acc1.withdraw(140);
+acc1.requestLoan(1000);
+acc1._approveLoan(1000);
+console.log(acc1.getMovements());
+console.log(acc1);
+
+// console.log(acc1.#movements);
+// console.log(acc1.#pin);
+// console.log(acc1.#approveLoan(100));
+
+Account.helper();
