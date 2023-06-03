c29386a127ed9eec354f933597684922d3925d03
NajirSeikh <ns.soft.97@gmail.com>
2023-06-03 10:35:40 +0530

    Chaining Methods

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -605,7 +605,7 @@ console.log(acc1._pin);
 
 */
 /* **************Encapsulation: Private Class Fields and Methods - 3/06/2023************** */
-
+/*
 // 1) Public fields
 // 2) Private fields
 // 3) Public methods
@@ -681,3 +681,89 @@ console.log(acc1);
 // console.log(acc1.#approveLoan(100));
 
 Account.helper();
+
+
+*/
+/* **************Chaining Methods - 3/06/2023************** */
+
+// 1) Public fields
+// 2) Private fields
+// 3) Public methods
+// 4) Private methods
+// (there is also the static veersion)
+
+class Account {
+  // 1) Public fields (instances)
+  locale = navigator.language;
+
+  // 2) Private fields (instances)
+  #movements = [];
+  #pin;
+
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    // Protected property
+    this.#pin = pin;
+    // this._movements = [];
+    // this.locale = navigator.language;
+
+    console.log(`Thanks for opening an account, ${owner}`);
+  }
+
+  // 3) Public methods
+
+  // Public intereface
+  getMovements() {
+    return this.#movements;
+  }
+
+  deposit(val) {
+    this.#movements.push(val);
+    return this;
+  }
+
+  withdraw(val) {
+    this.deposit(-val);
+    return this;
+  }
+
+  requestLoan(val) {
+    // if (this.#approveLoan(val)) {
+    if (this._approveLoan(val)) {
+      this.deposit(val);
+      console.log('Loan approved');
+      return this;
+    }
+  }
+
+  static helper() {
+    console.log('Helper');
+  }
+
+  // 4) Private methods
+  // #approveLoan(val) {
+  _approveLoan(val) {
+    return true;
+  }
+}
+
+const acc1 = new Account('Najir', 'EUR', 1111);
+
+// acc1._movements.push(250);
+// acc1._movements.push(-150);
+acc1.deposit(250);
+acc1.withdraw(140);
+acc1.requestLoan(1000);
+console.log(acc1.getMovements());
+console.log(acc1);
+Account.helper();
+
+// console.log(acc1.#movements);
+// console.log(acc1.#pin);
+// console.log(acc1.#approveLoan(100));
+
+// Chaining
+acc1.deposit(300).deposit(500).withdraw(35).requestLoan(25000).withdraw(4000);
+
+console.log(acc1.getMovements());
