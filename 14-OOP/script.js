6728be1c28f676a77c2eed565554199b74c9eceb
NajirSeikh <ns.soft.97@gmail.com>
2023-06-03 09:52:35 +0530

    Another Class Example

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -475,7 +475,7 @@ manna.introduce();
 manna.calcAge();
 */
 /* **************Inheritance Between 'Classes': Object.create - 2/06/2023************** */
-
+/*
 const PersonProto = {
   calcAge() {
     console.log(`${this.firstName} is ${2023 - this.birthYear} years old`);
@@ -503,3 +503,49 @@ const jay = Object.create(StudentProto);
 jay.init('Jay Kumar', 2010, 'CSE');
 jay.introduce();
 jay.calcAge();
+*/
+/* **************Another Class Example - 3/06/2023************** */
+
+class Account {
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    this.pin = pin;
+    this.movements = [];
+    this.locale = navigator.language;
+
+    console.log(`Thanks for opening an account, ${owner}`);
+  }
+
+  // Public intereface
+  deposit(val) {
+    this.movements.push(val);
+  }
+
+  withdraw(val) {
+    this.deposit(-val);
+  }
+
+  approveLoan(val) {
+    return true;
+  }
+
+  requestLoan(val) {
+    if (this.approveLoan(val)) {
+      this.deposit(val);
+      console.log('Loan approved');
+    }
+  }
+}
+
+const acc1 = new Account('Najir', 'EUR', 1111);
+
+// acc1.movements.push(250);
+// acc1.movements.push(-150);
+acc1.deposit(250);
+acc1.withdraw(140);
+acc1.requestLoan(1000);
+acc1.approveLoan(1000);
+
+console.log(acc1);
+console.log(acc1.pin);
