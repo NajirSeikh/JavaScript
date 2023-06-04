9ca719eed67396a98873c820b9a679932d6a7849
NajirSeikh <ns.soft.97@gmail.com>
2023-06-04 10:38:02 +0530

    Chaining Promises

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -5,6 +5,29 @@
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
+const renderCountry = function (data, className = '') {
+  const html = `
+  <article class="country ${className}">
+    <img class="country__img" src="${data?.flags?.png}" />
+    <div class="country__data">
+      <h3 class="country__name">${data.name?.official}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${(
+        +data.population / 1000000
+      ).toFixed(1)} people</p>
+      <p class="country__row"><span>🗣️</span>${
+        Object.entries(data.languages)[0][1]
+      }</p>
+      <p class="country__row"><span>💰</span>${
+        Object.entries(data.currencies)[0][1].name
+      }</p>
+    </div>
+  </article>
+  `;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
 /*
 const getCountryData = function (country) {
   const request = new XMLHttpRequest();
@@ -109,30 +132,6 @@ getCountryData('portugal');
 // getCountryData('india');
 */
 /* **************Welcome to Callback Hell - 04/06/2023************** */
-
-const renderCountry = function (data, className = '') {
-  const html = `
-  <article class="country ${className}">
-    <img class="country__img" src="${data?.flags?.png}" />
-    <div class="country__data">
-      <h3 class="country__name">${data.name?.official}</h3>
-      <h4 class="country__region">${data.region}</h4>
-      <p class="country__row"><span>👫</span>${(
-        +data.population / 1000000
-      ).toFixed(1)} people</p>
-      <p class="country__row"><span>🗣️</span>${
-        Object.entries(data.languages)[0][1]
-      }</p>
-      <p class="country__row"><span>💰</span>${
-        Object.entries(data.currencies)[0][1].name
-      }</p>
-    </div>
-  </article>
-  `;
-  countriesContainer.insertAdjacentHTML('beforeend', html);
-  countriesContainer.style.opacity = 1;
-};
-
 /*
 const getCountryAndNeighbour = function (country) {
   // AJAX Call Country 1
@@ -195,7 +194,7 @@ const request = fetch(`https://restcountries.com/v3.1/name/india`);
 console.log(request);
 */
 /* **************Consuming Promises - 04/06/2023************** */
-
+/*
 // const getCountryData = function (country) {
 //   fetch(`https://restcountries.com/v3.1/name/${country}`)
 //     .then(function (Response) {
@@ -215,4 +214,25 @@ const getCountryData = function (country) {
     .then(data => renderCountry(data[0]));
 };
 
+getCountryData('india');
+*/
+/* **************Chaining Promises - 04/06/2023************** */
+const getCountryData = function (country) {
+  // Country 1
+  fetch(`https://restcountries.com/v3.1/name/${country}`)
+    .then(Response => Response.json())
+    .then(data => {
+      renderCountry(data[0]);
+      const neighbour = data[0].borders[0];
+
+      if (!neighbour) return;
+
+      // Country 2
+      return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`);
+    })
+    .then(Response => Response.json())
+    .then(data => renderCountry(data[0], 'neighbour'));
+};
+
+// getCountryData('usa');
 getCountryData('india');
