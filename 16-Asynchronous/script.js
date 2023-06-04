d531472790c71a83e5d76cee860e06ee05dccbd0
NajirSeikh <ns.soft.97@gmail.com>
2023-06-04 16:02:40 +0530

    Throwing Errors Manually

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -33,6 +33,14 @@ const renderError = function (msg) {
   // countriesContainer.style.opacity = 1;
 };
 
+const getJSON = function (url, errorMsg = 'Something went wrong') {
+  return fetch(url).then(response => {
+    if (!response.ok) throw new Error(`${errorMsg} (${response.status})`);
+
+    return response.json();
+  });
+};
+
 /*
 const getCountryData = function (country) {
   const request = new XMLHttpRequest();
@@ -245,7 +253,7 @@ getCountryData('india');
 
 */
 /* **************Handling Rejected Promises - 04/06/2023************** */
-
+/*
 const getCountryData = function (country) {
   // Country 1
   fetch(`https://restcountries.com/v3.1/name/${country}`)
@@ -273,3 +281,40 @@ const getCountryData = function (country) {
 btn.addEventListener('click', function () {
   getCountryData('india');
 });
+getCountryData('dskjkjsbk');
+
+*/
+/* **************Throwing Errors Manually - 04/06/2023************** */
+
+const getCountryData = function (country) {
+  // Country 1
+  getJSON(`https://restcountries.com/v3.1/name/${country}`, 'Country not found')
+    .then(data => {
+      renderCountry(data[0]);
+      const neighbour = data[0].borders[0];
+
+      if (!neighbour) throw new Error('No neighbour found!');
+
+      // Country 2
+      return getJSON(
+        `https://restcountries.com/v3.1/alpha/${neighbour}`,
+        'Country not found'
+      );
+    })
+
+    .then(data => renderCountry(data[0], 'neighbour'))
+    .catch(err => {
+      console.error(`${err} 💥💥💥`);
+      renderError(`Something went wrong 💥💥 ${err.message}. Try again!`);
+    })
+    .finally(() => {
+      countriesContainer.style.opacity = 1;
+    });
+};
+
+btn.addEventListener('click', function () {
+  getCountryData('india');
+  getCountryData('portugal');
+});
+
+// getCountryData('australia');
