edb547f1e79ad2ad3607e76ca98842024f8b05f3
NajirSeikh <ns.soft.97@gmail.com>
2023-06-04 09:11:49 +0530

    Our First AJAX Call: XMLHttpRequest - updated

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -35,37 +35,76 @@ getCountryData('usa');
 getCountryData('germany');
 */
 
+// const btn = document.querySelector('.btn-country');
+// const countriesContainer = document.querySelector('.countries');
+
+// const request = new XMLHttpRequest();
+// request.open('GET', 'https://restcountries.com/v3.1/name/india');
+// // request.open('GET', 'https://restcountries.com/v3.1/name/usa');
+// request.send();
+// // console.log(request.responseText);
+
+// request.addEventListener('load', function () {
+//   // console.log(this.responseText);
+
+//   const [data] = JSON.parse(this.responseText);
+//   console.log(data);
+
+//   const html = `
+//   <article class="country">
+//           <img class="country__img" src="${data?.flags?.png}" />
+//           <div class="country__data">
+//             <h3 class="country__name">${data?.name?.common}</h3>
+//             <h6 class="country__region">${data?.capital}</h6>
+//             <h4 class="country__region">${data?.region}</h4>
+//             <p class="country__row"><span>👫</span>${(
+//               +data.population / 1000000
+//             ).toFixed(1)}m people</p>
+//             <p class="country__row"><span>🗣️</span>${data?.languages?.hin}</p>
+//             <p class="country__row"><span>💰</span>${
+//               data?.currencies?.INR?.name
+//             }</p>
+//           </div>
+//         </article>`;
+//   countriesContainer.insertAdjacentHTML('beforeend', html);
+//   countriesContainer.style.opacity = 1;
+// });
+
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
-const request = new XMLHttpRequest();
-request.open('GET', 'https://restcountries.com/v3.1/name/india');
-// request.open('GET', 'https://restcountries.com/v3.1/name/usa');
-request.send();
-// console.log(request.responseText);
+const getCountryData = function (country) {
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
 
-request.addEventListener('load', function () {
-  // console.log(this.responseText);
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
 
-  const [data] = JSON.parse(this.responseText);
-  console.log(data);
-
-  const html = `
+    const html = `
   <article class="country">
-          <img class="country__img" src="${data?.flags?.png}" />
-          <div class="country__data">
-            <h3 class="country__name">${data?.name?.common}</h3>
-            <h6 class="country__region">${data?.capital}</h6>
-            <h4 class="country__region">${data?.region}</h4>
-            <p class="country__row"><span>👫</span>${(
-              +data.population / 1000000
-            ).toFixed(1)}m people</p>
-            <p class="country__row"><span>🗣️</span>${data?.languages?.hin}</p>
-            <p class="country__row"><span>💰</span>${
-              data?.currencies?.INR?.name
-            }</p>
-          </div>
-        </article>`;
-  countriesContainer.insertAdjacentHTML('beforeend', html);
-  countriesContainer.style.opacity = 1;
-});
+    <img class="country__img" src="${data?.flags?.png}" />
+    <div class="country__data">
+      <h3 class="country__name">${data.name?.official}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${(
+        +data.population / 1000000
+      ).toFixed(1)} people</p>
+      <p class="country__row"><span>🗣️</span>${
+        Object.entries(data.languages)[0][1]
+      }</p>
+      <p class="country__row"><span>💰</span>${
+        Object.entries(data.currencies)[0][1].name
+      }</p>
+    </div>
+  </article>
+  `;
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+  });
+};
+
+getCountryData('usa');
+getCountryData('portugal');
+// getCountryData('india');
