d47d99ad6313e1e2a9ec1ae6977f01d57c37d34d
NajirSeikh <ns.soft.97@gmail.com>
2023-06-21 07:35:45 +0530

    Numeric Separators

--- 12-Numbers-Dates-Timers-Bankist/script.js
+++ 12-Numbers-Dates-Timers-Bankist/script.js
@@ -334,7 +334,7 @@ console.log((2.345).toFixed(2)); //2.35
 console.log(+(2.345).toFixed(2)); //2.35 (its a number bcoz of '+')
 */
 /* **************The Reminder Operator - 20/06/2023************** */
-
+/*
 console.log(5 % 2); //1
 console.log(5 / 2); // 2.5, 5 = 2 * 2 +1
 
@@ -362,3 +362,29 @@ labelBalance.addEventListener('click', function () {
     if (i % 3 === 0) row.style.backgroundColor = 'blue';
   });
 });
+
+*/
+/* **************Numeric Separators - 21/06/2023************** */
+
+// 287,460,000,000
+const diameter = 287_460_000_000;
+console.log(diameter); //287460000000
+
+const price = 345_99;
+console.log(price); // 34500
+
+const transferFee1 = 15_00;
+console.log(transferFee1); //1500
+const transferFee2 = 1_500;
+console.log(transferFee2); //1500
+
+const PI = 3.1415;
+console.log(PI); //3.1415
+// This is not allowed
+// const PI = 3_.1415;
+// const PI = 3.1415_;
+// const PI = 3._1415;
+// const PI = _3.1415;
+
+console.log(Number('230_00')); //NaN
+console.log(parseInt('230_00')); //230
