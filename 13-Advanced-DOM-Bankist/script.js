1540abe16bada38f5fed7f05a0392aa0b0e9b4f8
NajirSeikh <ns.soft.97@gmail.com>
2023-07-01 15:58:59 +0530

    Selecting, Creating, and Deleting Elements

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -33,3 +33,40 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+/* **************Selecting, Creating, and Deleting Elements - 01/07/2023************** */
+// Selecting elements
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+const header = document.querySelector('.header');
+const allSections = document.querySelectorAll('.section');
+console.log(allSections);
+
+document.getElementById('section--1');
+// console.log(document.getElementById('section--1'));
+const allButtons = document.getElementsByTagName('button');
+console.log(allButtons);
+
+console.log(document.getElementsByClassName('btn'));
+
+// Creating and inserting elements
+// .insertAdjacentHTML
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+// message.textContent = 'We use cookied for improved functionality and analytics';
+message.innerHTML =
+  'We use cookied for improved functionality and analytics. <button class="btn btn--close-cookie" >Got it!</button>';
+
+// header.prepend(message); // add to first position
+header.append(message); // add to last position
+// header.append(message.cloneNode(true)); // add to last position and clone it
+// header.before(message); // add before header
+// header.after(message); // add after header
+
+// Delete elements
+document.querySelector('.btn--close-cookie').addEventListener('click', () => {
+  // message.remove();
+  message.parentElement.removeChild(message);
+});
